0bd18d80d76af5c4cc13c0c9b09bd3004b5dfde2
Muzammil803 <syedmuzammil691@gmail.com>
2022-08-01 19:57:02 +0500

    done

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+
+document.getElementById("back")
+
+
+function redlighton(){
+    var color = document.getElementById("red")
+     color.style.backgroundColor="red"
+  
+    }
+
+ function redlightoff(){
+    var color = document.getElementById("red")
+     color.style.backgroundColor="white"
+ }
+
+
+ function yellowlighton(){
+    var color = document.getElementById("yellow")
+     color.style.backgroundColor="yellow"
+  
+    }
+
+ function yellowlightoff(){
+    var color = document.getElementById("yellow")
+     color.style.backgroundColor="white"
+ }
+
+
+  
+ function greenlighton(){
+    var color = document.getElementById("green")
+     color.style.backgroundColor="green"
+  
+    }
+
+ function greenlightoff(){
+    var color = document.getElementById("green")
+     color.style.backgroundColor="white"
+ }
+
+
+  
